e0fff54cee5ec512386b4fb072f614187c6df9ee
YukiOkamoto <yuuki.okamoto.it.fitness@gmail.com>
2020-05-23 21:58:34 +0900

    Navigate Between Pages

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,12 @@
+import Link from 'next/link'
+
 export default () => (
-  <h1>First Post</h1>
-)
+  <>
+    <h1>First Post</h1>
+    <h2>
+      <Link href="/">
+        <a>Back to home</a>
+      </Link>
+    </h2>
+  </>
+);
